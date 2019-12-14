d8011580b64f055da3b55f5c8ce5c179e7d31b47
brownguo <yexuan.guo@codedance.cn>
2019-12-14 17:12:22 +0800

    fixed..

--- core/Worker.php
+++ core/Worker.php
@@ -18,7 +18,7 @@ class Worker
         self::installSignal();
         #self::for_one_worker();
         self::createWorkers();
-        self::run();
+        #self::run();
     }
 
     protected static function run()
@@ -58,9 +58,9 @@ class Worker
         if (!empty($title))
         {
             // 需要扩展
-            if(extension_loaded('proctitle') && function_exists('setproctitle'))
+            if(!extension_loaded('proctitle') && function_exists('setproctitle'))
             {
-                @setproctitle($title);
+                setproctitle($title);
             }
             // >=php 5.5
             elseif (function_exists('cli_set_process_title'))
@@ -70,9 +70,26 @@ class Worker
         }
     }
 
+    protected static function createChannel()
+    {
+        // 建立进程间通信通道，目前是用unix域套接字
+        $channel = stream_socket_pair(STREAM_PF_UNIX, STREAM_SOCK_STREAM, STREAM_IPPROTO_IP);
+        if(false === $channel)
+        {
+            return false;
+        }
+
+        stream_set_blocking($channel[0], 0);
+        stream_set_blocking($channel[1], 0);
+        return $channel;
+    }
+
     protected static function createWorkers()
     {
-        for($i=1;$i<=5;$i++)
+
+        var_dump(self::createChannel());
+        exit(0);
+        for($i=1;$i<=2;$i++)
         {
             #echo $i.PHP_EOL;
             self::for_one_worker($i);
@@ -82,18 +99,19 @@ class Worker
     {
         $pid = pcntl_fork();
 
-        //主进程记录子进程pid
+        //主进程记录子进程pid,当前上线文为Master
         if($pid > 0)
         {
-            self::set_process_title(sprintf('PHPServerd Pid[%s]',$pid));
             self::$worker_pids[$worker_id] = $pid;
         }
-        //子进程运行
+        //子进程运行,当前上下文为Worker
         elseif($pid == 0)
         {
+            file_put_contents('../logs/wechat_bot.log',date('Y-m-d H:i:s').'WorkerPid['.posix_getpid().']'.PHP_EOL,FILE_APPEND);
+            #self::set_process_title(sprintf('PHPServerd Pid[%s]',$pid));
             exit(0);
         }
-        //出错退出
+        //出错退出,-1
         else
         {
             exit('fork worker fail!');
